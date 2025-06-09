45e8565509574f21699f003722b634d2764a79f4
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-09 20:11:39 +0300

    inprimary commit

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
-from app import create_app
+from app import create_app, db
+from app.models import User, Device, Sale
+
 app = create_app()
-#     return app
-# if __name__ == "__main__":
-#     app.run(debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    """Add database models to flask shell context"""
+    return {
+        'db': db,
+        'User': User,
+        'Device': Device,
+        'Sale': Sale
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
