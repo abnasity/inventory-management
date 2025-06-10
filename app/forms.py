54bd47c8fd57df0db146c98ed4ca81a4f67a5db3
Kipngeno Abednego <abnasgit@gmail.com>
2025-06-10 10:40:07 +0300

    add admin details

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,32 @@ from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class ProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
+    current_password = PasswordField('Current Password')
+    new_password = PasswordField('New Password')
+    confirm_password = PasswordField('Confirm New Password')
+    submit = SubmitField('Update Profile')
+
+    def __init__(self, original_username, original_email, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+        self.original_email = original_email
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already in use. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != self.original_email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already registered. Please use a different one.')
